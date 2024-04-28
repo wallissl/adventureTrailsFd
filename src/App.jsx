cd069caae9887ac6b59009bdbcbe62c7dbec1a3e
Wallis <wallis.sl@outlook.com>
2024-04-28 14:56:48 -0300

    Ex4

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,7 @@ function App(){
         }  - Chamando o componenete local */ }
 
 
-        <HookUseEffect />
+        {/* <HookUseEffect /> */}
 
         
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,62 @@ function Home(){
     return(
         <>
         
-        <h1> Home </h1>
+{/*         <h1> Home </h1>
+ */}        <div className="container1">
+
+            <h3>Que tal aproveitar um tempo com a natureza?</h3>
+
+            <div className="text1">Juste-se à comunidade de entusiastas ao ar livre, compartilhe suas aventuras e inspire-se com as experiências de outros aventureiros.
+            Prepare-se para explorar novos horizontes e se conectar com a natureza através do Adventure Trails!
+            </div>
+
+            <button> Explorar Trilhas </button>
+
+
+        </div>
+
+        <div className="container2">
+
+            <h2>Explore trilhas incríveis</h2>
+
+            <div className="text2">O "Adventure Trails FD" é o seu portal para explorar e compartilhar as melhores trilhas para trekking e ciclismo ao redor do mundo.
+            Descubra todas deslumbrantes, desde caminhos tranquilos por florestas exuberantes até trilhas desafiadoras em montanhas majestosas. Encontre informações detalhadas sobre cada trilha, incluindo distância, dificuldade, pontos de interesse naturais e dicas úteis para uma experiência eco-friendly.
+            </div>
+
+            <button> Explorar Trilhas </button>
+
+        </div>
+
+        <div className="container2">
+
+            <h2>Explore trilhas incríveis</h2>
+
+            <div className="text2">O "Adventure Trails FD" é o seu portal para explorar e compartilhar as melhores trilhas para trekking e ciclismo ao redor do mundo.
+            Descubra todas deslumbrantes, desde caminhos tranquilos por florestas exuberantes até trilhas desafiadoras em montanhas majestosas. Encontre informações detalhadas sobre cada trilha, incluindo distância, dificuldade, pontos de interesse naturais e dicas úteis para uma experiência eco-friendly.
+            </div>
+
+            <button> Explorar Trilhas </button>
+
+        </div>
+
+
+        <div className="container3">
+
+            <div className="bl1">
+
+                <div className="textg">Compartilhe fotos, dicas e localização das suas trilhas favoritas</div>
+
+                <button className="bth">Cadastrar nova trilha</button>
+
+            </div>
+            
+            <div className="bl2">
+            <img src="https://images.pexels.com/photos/917510/pexels-photo-917510.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="imh"></img>
+            </div>
+
+
+        </div>
+
         
         </>
     )
